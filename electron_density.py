1a78144f9348129f1c8092d05bc2652fb21b8e45
Gemechu Fanta Garuma <45550456+gemechufanta@users.noreply.github.com>
2022-07-19 08:52:47 +0300

    new file

--- electron_density.py
+++ electron_density.py
@@ -1,14 +1,13 @@
 # coding: utf-8
 import pandas as pd
 ds = pd.read_csv('noaa_goes13_epead_electron_flux_a_5m.csv')
-ds
 ds.columns=['time','electron_flux']
+reference_date = '1970-01-01'
+ds['time'] = pd.date_range(start=reference_date, periods=len(ds.time), freq='L')
 ds.time 
 time = ds.time 
 ef=ds.electron_flux
-import matplotlib.pyplot as plt
-plt.plot(time, ef)
+import matplotlib.pyplot as plt 
+plt.plot(time[ef > 0], ef[ef > 0]) 
+plt.savefig("electron_density.png")
 plt.show() 
-plt.savefig("electron_density.png")
-plt.plot(time, ef)
-plt.savefig("electron_density.png")
